437a4dcc16b344b1338d3e35eb38347af09f5192
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-09 23:36:30 -0400

    Diferent colors and no debug for lighttouch

--- log.lua
+++ log.lua
@@ -13,14 +13,13 @@ log.usecolor = true
 log.outfile = nil
 log.level = "trace"
 
-
-local modes = {
-  { name = "trace", color = "\27[34m", },
-  { name = "debug", color = "\27[36m", },
-  { name = "info",  color = "\27[32m", },
-  { name = "warn",  color = "\27[33m", },
-  { name = "error", color = "\27[31m", },
-  { name = "fatal", color = "\27[35m", },
+local modes = { -- \27[30m is for black text \27[48;5;numberm are for background according to this site http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+  { name = "trace", color = "\27[30m\27[48;5;255m", },
+  { name = "debug", color = "\27[30m\27[48;5;27m", },
+  { name = "info",  color = "\27[30m\27[48;5;33m", },
+  { name = "warn",  color = "\27[30m\27[48;5;226m", },
+  { name = "error", color = "\27[30m\27[48;5;208m", },
+  { name = "fatal", color = "\27[30m\27[48;5;196m", },
 }
 
 
@@ -61,15 +60,16 @@ for i, x in ipairs(modes) do
       return
     end
 
-    local msg = tostring(...)
-    local info = debug.getinfo(2, "Sl")
-    local lineinfo = info.short_src .. ":" .. info.currentline
+    local msg = ...
+    --local info = debug.getinfo( 2, "Sl" )
+    --local lineinfo = info.short_src .. ":" .. info.currentline
+    local lineinfo = ""
 
     -- Output to console
-    print(string.format("%s[%-6s%s]%s %s: %s",
+    print(string.format("%s %s %s %s %s %s",
+                        os.date("%H:%M:%S"),
                         log.usecolor and x.color or "",
                         nameupper,
-                        os.date("%H:%M:%S"),
                         log.usecolor and "\27[0m" or "",
                         lineinfo,
                         msg))
